8ba416d07414ced01ad91661d3fe4d88c53f9bd8
Nick Ito <u.skayyyyy@gmail.com>
2024-08-18 21:59:40 -0700

    feat (server): add express server boilerplate

---
+++ server/src/app.ts
@@ -0,0 +1,17 @@
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
+
+export const app = express();
+
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+	})
+);
+app.use(morgan("combined"));
+app.use(express.json());
+
+app.get("/", (_, res) => {
+	res.send("Hello World!");
+});
